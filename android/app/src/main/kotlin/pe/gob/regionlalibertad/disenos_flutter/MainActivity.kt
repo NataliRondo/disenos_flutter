3767a9a06d8795505549f40ea12bf598f92a1bcb
NataliRondo <51837916+NataliRondo@users.noreply.github.com>
2024-01-03 12:00:32 -0500

    first commit

---
+++ android/app/src/main/kotlin/pe/gob/regionlalibertad/disenos_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package pe.gob.regionlalibertad.disenos_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,263 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: const Color(0xff615AAB),
+    );
+  }
+}
+
+class HeaderCircular extends StatelessWidget {
+  const HeaderCircular({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: const BoxDecoration(
+          color: Color(0xff615AAB),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(50),
+            bottomRight: Radius.circular(50),
+          )),
+    );
+  }
+}
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      //color: const Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    //propiedades
+
+    paint.color = const Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20;
+
+    final path = Path();
+
+    //dibujar con el path y paint
+
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.35);
+    path.lineTo(size.width,0);
+    path.lineTo(0, 0);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderTriangulo extends StatelessWidget {
+  const HeaderTriangulo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      //color: const Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderTrianguloPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderTrianguloPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    //propiedades
+
+    paint.color = const Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 20;
+
+    final path = Path();
+
+    //dibujar con el path y paint
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderPico extends StatelessWidget {
+  const HeaderPico({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderPicoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderPicoPainter extends CustomPainter {
+  
+  @override
+  void paint(Canvas canvas, Size size) {
+    
+    final lapiz = Paint();
+
+    // Propiedades
+    lapiz.color = const Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; // .fill .stroke
+    lapiz.strokeWidth = 20;
+
+    final path = Path();
+
+    // Dibujar con el path y el lapiz
+    path.lineTo( 0, size.height * 0.25 );
+    path.lineTo( size.width * 0.5, size.height * 0.30 );
+    path.lineTo( size.width, size.height * 0.25 );
+    path.lineTo( size.width, 0 );
+  
+
+
+    canvas.drawPath(path, lapiz );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+
+}
+
+class HeaderCurvo extends StatelessWidget {
+  const HeaderCurvo({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderCurvoPainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderCurvoPainter extends CustomPainter {
+  
+  @override
+  void paint(Canvas canvas, Size size) {
+    
+    final lapiz = Paint();
+
+    // Propiedades
+    lapiz.color = const Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; // .fill .stroke
+    lapiz.strokeWidth = 20;
+
+    final path = Path();
+
+    // Dibujar con el path y el lapiz
+    path.lineTo( 0, size.height * 0.25 );
+    path.quadraticBezierTo(size.width * 0.5, size.height * 0.40, size.width, size.height * 0.25 );
+    path.lineTo( size.width, 0 );
+
+  
+
+
+    canvas.drawPath(path, lapiz );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+
+}
+
+
+class HeaderWave extends StatelessWidget {
+  const HeaderWave({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderWavePainter(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavePainter extends CustomPainter {
+  
+  @override
+  void paint(Canvas canvas, Size size) {
+    
+    final lapiz = Paint();
+
+    // Propiedades
+    lapiz.color = const Color(0xff615AAB);
+    lapiz.style = PaintingStyle.fill; // .fill .stroke
+    lapiz.strokeWidth = 20;
+
+    final path = Path();
+
+    // Dibujar con el path y el lapiz
+    path.lineTo( 0, size.height * 0.25 );
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.30, size.width * 0.5, size.height * 0.25 );
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.20, size.width, size.height * 0.25 );
+    path.lineTo( size.width, 0 );
+
+  
+
+
+    canvas.drawPath(path, lapiz );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+
+}
